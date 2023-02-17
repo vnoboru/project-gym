5ff8d384b8abb9dcd3970c9b0cc44932feebebf3
Vitor Torii <vitortorii@gmail.com>
2023-02-17 03:13:00 -0300

    feat: Config Prettier/ESLint and Husky

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,8 +2,7 @@ import dotenv from "dotenv";
 import dotenvExpand from "dotenv-expand";
 
 export function loadEnv() {
-  const path =
-    process.env.NODE_ENV === "development" ? ".env.development" : ".env";
+  const path = process.env.NODE_ENV === "development" ? ".env.development" : ".env";
 
   const currentEnvs = dotenv.config({ path });
   dotenvExpand.expand(currentEnvs);
